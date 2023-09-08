1cea9706814840cb94e7b8a05cfe483454496924
kyawzin369 <kyawzinthet689@gmail.com>
2023-09-08 10:13:16 +0630

    Update In A Box

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,7 +60,6 @@ sr.reveal(`.home__data, .home__img,
             .about__data, .about__img,
             .services__content, .menu__content,
             .app__data, .app__img,
-            .contact__data, .contact__button,
-            .footer__content`, {
+            .contact__data, .contact__button`, {
     interval: 200
 })
